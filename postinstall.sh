9962dee89d1726df15b7ded18db5d1c638febd7d
Kyle Bowerman <kyle.bowerman@appirio.com>
2014-10-17 15:09:30 -0500

    forgot post install file

---
+++ postinstall.sh
@@ -0,0 +1,13 @@
+# install bower on edit
+cd client/edit-challenge
+../../node_modules/bower/bin/bower install
+
+cd ../manage-challenge/
+../../node_modules/bower/bin/bower install
+
+cd ../..
+
+
+#clean
+#rm -rf client/edit-challenge/bower_components
+#rm -rf client/manage-challenge/bower_components
