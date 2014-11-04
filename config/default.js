b83c5212c47c6d5f859aacfa3035b60657fe62bc
Kyle Bowerman <kyle.bowerman@appirio.com>
2014-11-04 14:43:54 -0600

    updated to use config.get and added challengeServiceURI, also had to force an update to jquery for tags

--- config/default.js
+++ config/default.js
@@ -1,7 +1,10 @@
 'use strict';
 
 module.exports = {
-  auth0Domain: process.env.TC_AUTH0_DOMAIN || 'serenity-tc.auth0.com',
-  auth0Client: process.env.TC_AUTH0_CLIENT || 'foo',
-  auth0Secret: process.env.TC_AUTH0_SECRET || 'bar'
+  challengeServiceURI: 'http://lc1-challenge-service.herokuapp.com/',
+  auth0: {
+    Domain: process.env.TC_AUTH0_DOMAIN || 'serenity-tc.auth0.com',
+    Client: process.env.TC_AUTH0_CLIENT || 'foo',
+    Secret: process.env.TC_AUTH0_SECRET || 'bar'
+  }
 };
